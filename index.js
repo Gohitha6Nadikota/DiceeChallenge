acf433aa678b406bb60ba807e50ef1ff70c86e4a
Gohitha6Nadikota <116640972+Gohitha6Nadikota@users.noreply.github.com>
2023-05-25 11:11:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;
+var filename1="dice"+randomNumber1+".png";
+document.querySelectorAll("img")[0].setAttribute("src",filename1);
+
+var randomNumber2=Math.floor(Math.random()*6)+1;
+var filename2="dice"+randomNumber2+".png";
+document.querySelectorAll("img")[1].setAttribute("src",filename2);
+
+if(randomNumber1>randomNumber2)
+    document.querySelector("h1").innerHTML="🚩Player 1 wins";
+else if(randomNumber2>randomNumber1)
+    document.querySelector("h1").innerHTML="Player 2 wins🚩";
+else
+    document.querySelector("h1").innerHTML="Draw!";
